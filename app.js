03e339d7cb821376361a40a08a0f7cc868262fef
unknown <dariensloan@gmail.com>
2018-11-29 16:18:47 -0500

    add proper ip

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.get('*', function(req,res){
 });
 
 
-app.listen(3000, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log('Listening on Localhost');
 });
